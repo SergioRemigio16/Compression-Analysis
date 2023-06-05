32a77fbe31f543b970e5942b9180137fdb1234d3
SergioRemigio <35435983+SergioRemigio@users.noreply.github.com>
2023-06-05 16:44:02 -0600

    Added FFTWTesting file that will start measuring how well the FFT compression and decompression algorithm performs

---
+++ WindowsVisualStudioSolution/CDF/CompressionDecompressionFramework/TimingExperiment.h
@@ -0,0 +1,31 @@
+#ifndef ZFP_TESTING_H
+#define ZFP_TESTING_H
+
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <zfp.h>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include "Utilities.h"
+#include "CompressionDecompression.h"
+
+#define RUNS 1000
+#define WARMUP_RUNS 100
+
+// Calculate size of a double vector
+size_t calculateSize(const std::vector<double>& matrix);
+
+// Calculate size of a compression result
+size_t calculateSize(const CompressionResult& compressionResult);
+
+// Run experiment for different compression rates
+void runExperiment(int x, int y, int z, bool useWave, bool visualizeData, int rate);
+
+// Run experiments for a range of rates
+void runExperimentsForRates(int x, int y, int z, bool useWave, bool visualizeData, int minRate, int maxRate);
+
+int runTimingExperiment();
+
+#endif 
